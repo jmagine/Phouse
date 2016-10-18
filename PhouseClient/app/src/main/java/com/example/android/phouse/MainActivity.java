629e2a9fb17787f236ab38fd8d35f2f539919cba
ThisChessPlayer <jasonma5501@gmail.com>
2016-10-17 17:33:43 -0700

    fixed socket timeout, added strings in resources to get rid of most warnings, changed toast position, added zero movement area

--- PhouseClient/app/src/main/java/com/example/android/phouse/MainActivity.java
+++ PhouseClient/app/src/main/java/com/example/android/phouse/MainActivity.java
@@ -9,6 +9,7 @@ import android.os.StrictMode;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
@@ -21,7 +22,9 @@ import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.net.SocketTimeoutException;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -34,13 +37,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
   private boolean isConnected = false;
   private float   orientation[] = new float[3];
-  private int     sensVal = 50;
+
+  private int zeroThres = 5;
+
+  private int     sensVal = 500;
 
   private Socket      socket;
   private PrintWriter out;
-
-  private SensorManager sensorM;
-  private Sensor        accSensor;
 
   private float xCal;
   private float yCal;
@@ -50,13 +53,37 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onSensorChanged(SensorEvent event) {
       if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
-        orientation[0] = event.values[0];
-        orientation[1] = event.values[1];
-        orientation[2] = event.values[2];
+        float norm = (float) Math.sqrt(event.values[0] * event.values[0] +
+                                event.values[1] * event.values[1] +
+                                event.values[2] * event.values[2]);
+
+        orientation[0] = event.values[0] / norm;
+        orientation[1] = event.values[1] / norm;
+        orientation[2] = event.values[2] / norm;
+
+        float xDisp = (orientation[0] - xCal) * -1;
+        float yDisp = (orientation[1] - yCal);
+
+        float zeroSpace = (float) zeroThres / 50;
+        if(xDisp > -1 * zeroSpace && xDisp < zeroSpace)
+          xDisp = 0;
+        else if(xDisp >= zeroSpace)
+          xDisp -= zeroSpace;
+        else
+          xDisp += zeroSpace;
+
+        if(yDisp > -1 * zeroSpace && yDisp < zeroSpace)
+          yDisp = 0;
+        else if(yDisp >= zeroSpace)
+          yDisp -= zeroSpace;
+        else
+          yDisp += zeroSpace;
+
 
         if(out != null)
-          out.println((float) sensVal / 50 * (orientation[0] - xCal) * -1 + "," + (float) sensVal / 50 * (orientation[1] - yCal));
+          out.println((float) sensVal / 100 * xDisp + "," + (float) sensVal / 100 * yDisp);
       }
+
     }
 
     @Override
@@ -78,10 +105,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     context = this;
     updates = new Toast(context);
+    updates.cancel();
+    updates.setGravity(Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL, 16, 0);
 
     //initialize accelerometer/sensor manager/listener
-    sensorM = (SensorManager) getSystemService(SENSOR_SERVICE);
-    accSensor = sensorM.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+    SensorManager sensorM = (SensorManager) getSystemService(SENSOR_SERVICE);
+    Sensor accSensor = sensorM.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
     sensorM.registerListener(accListener, accSensor, SensorManager.SENSOR_DELAY_FASTEST);
 
     connectButton = (Button) findViewById(R.id.connectButton);
@@ -94,9 +123,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if(isConnected && out != null) {
           switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
+              findViewById(R.id.lclickButton).setBackgroundColor(0xFF00FFFF);
               out.println("left_click_down");
               break;
             case MotionEvent.ACTION_UP:
+              findViewById(R.id.lclickButton).setBackgroundColor(0xFF00CFFF);
               out.println("left_click_up");
               break;
           }
@@ -110,9 +141,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if(isConnected && out != null) {
           switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
+              findViewById(R.id.rclickButton).setBackgroundColor(0xFF00FFFF);
               out.println("right_click_down");
               break;
             case MotionEvent.ACTION_UP:
+              findViewById(R.id.rclickButton).setBackgroundColor(0xFF00CFFF);
               out.println("right_click_up");
               break;
           }
@@ -126,6 +159,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
       @Override
       public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
         sensVal = progress;
+      }
+
+      @Override
+      public void onStartTrackingTouch(SeekBar seekBar) {
+
+      }
+
+      @Override
+      public void onStopTrackingTouch(SeekBar seekBar) {
+
+      }
+    });
+    SeekBar zeroThreshold = (SeekBar) findViewById(R.id.zerothreshold);
+    zeroThreshold.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+      @Override
+      public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        zeroThres = progress;
       }
 
       @Override
@@ -153,6 +203,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
           //make attempt to connect
           updates.cancel();
           updates = Toast.makeText(context, "Connecting...", Toast.LENGTH_SHORT);
+          updates.setGravity(Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL, 16, 0);
           updates.show();
 
           ipAddr = ((EditText)findViewById(R.id.ip_address)).getText().toString();
@@ -162,6 +213,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
           } catch (NumberFormatException e){
             updates.cancel();
             updates = Toast.makeText(context, "Invalid socket. Enter a positive integer.", Toast.LENGTH_SHORT);
+            updates.setGravity(Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL, 16, 0);
             updates.show();
           }
 
@@ -170,6 +222,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
           //display result of attempt
           updates.cancel();
           updates = Toast.makeText(context,isConnected?"Connected to server!":"Error while connecting. Make sure the IP address is correct and the server is running.",Toast.LENGTH_LONG);
+          updates.setGravity(Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL, 16, 0);
           updates.show();
 
           //return on failure to connect
@@ -183,13 +236,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             out.println("connected");
 
             //update state of buttons and editText fields
-            connectButton.setText("Disconnect");
+            connectButton.setText(R.string.disconnect);
             findViewById(R.id.ip_address).setEnabled(false);
             findViewById(R.id.socket).setEnabled(false);
           }catch (IOException e){
             Log.e("remotedroid", "Error while creating OutWriter", e);
             updates.cancel();
             updates = Toast.makeText(context,"Error while connecting",Toast.LENGTH_LONG);
+            updates.setGravity(Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL, 16, 0);
             updates.show();
           }
         }
@@ -198,6 +252,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
           //make attempt to disconnect
           updates.cancel();
           updates = Toast.makeText(context,"Disconnecting...",Toast.LENGTH_SHORT);
+          updates.setGravity(Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL, 16, 0);
           updates.show();
           try {
             out.println("exit");
@@ -205,7 +260,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             //update state of variables and views
             isConnected = false;
-            connectButton.setText("Connect");
+            connectButton.setText(R.string.connect);
             findViewById(R.id.ip_address).setEnabled(true);
             findViewById(R.id.socket).setEnabled(true);
           } catch (IOException e) {
@@ -232,13 +287,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
   }
 
-  //TODO make ip and socket dynamically stated on connect mouseclick
+  //TODO load socket and ip from file
   public void setConnection(boolean state) {
     if(state) {
       try {
         InetAddress serverAddr = InetAddress.getByName(ipAddr);
-        socket = new Socket(serverAddr, socketNum); //Open socket on server IP and port
+        socket = new Socket(); //Open socket on server IP and port
+        socket.connect(new InetSocketAddress(serverAddr, socketNum), 1000);
         isConnected = true;
+      } catch (SocketTimeoutException e) {
+        try {
+          if (socket.isConnected()) {
+            socket.close();
+          }
+        }
+        catch (Exception e2) {
+          Log.e("remotedroid", "Error while closing connection", e2);
+        }
       } catch (IOException e) {
         Log.e("remotedroid", "Error while connecting", e);
       }
--- PhouseServer/ComputerPort.java
+++ PhouseServer/ComputerPort.java
@@ -13,135 +13,148 @@ import java.net.Socket;
 import java.util.Scanner;
  
 public class ComputerPort {
-  private BufferedReader in = null;
-  private ServerSocket server = null;
-  private Socket client = null;
-  private Robot robot;
-  private String line;
-  private boolean running = false;
-  private boolean isConnected=false;
-  private int serverPort = 8998;
-  private int height;
-  private int width;  
-
-  private float nowx;
-  private float nowy;
-
-  public ComputerPort() {
-    selectPort();
-    init();
-  }
-
-  private void selectPort() {
-    Scanner scan = new Scanner(System.in);
-    System.out.print("Please select a socket (e.g. 9000): ");
-    serverPort = Integer.parseInt(scan.next());
-    scan.close();
-  }
-
-  private void init() {
-    try{
-      System.out.println("[Server] INIT: Socket: " + serverPort);
-      Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-      width = screenSize.width;
-      height = screenSize.height;
-      robot = new Robot(); //create robot for automating mouse movements/clicks
-      server = new ServerSocket(serverPort); //create a server socket on port
-      Point point = MouseInfo.getPointerInfo().getLocation(); //Get current mouse position
-      nowx = point.x;
-      nowy = point.y;
-    }catch (IOException e) {
-      System.out.println("[Server] Error in opening Socket. Make sure no other program is using this socket!");
-      System.exit(-1);
-    }catch (AWTException e) {
-      System.out.println("[Server] Error in creating robot instance");
-      System.exit(-1);
-    }
-  }
+	private BufferedReader in = null;
+	private ServerSocket server = null;
+	private Socket client = null;
+	private Robot robot;
+	private String line;
+	private boolean running = false;
+	private boolean isConnected=false;
+	private int serverPort = 9000;
+	private int height;
+	private int width;
 	
-  public void updateState() {
-    running = true;
-    //read input from client while it is connected
-    while(running) {
-      if(!isConnected) {
-        try {
-          client = server.accept(); //listens for a connection to be made to this socket and accepts it
-          in = new BufferedReader(new InputStreamReader(client.getInputStream())); //the input stream where data will come from client
-          isConnected = true;
-          Point point = MouseInfo.getPointerInfo().getLocation(); //Get current mouse position
-          nowx = point.x;
-          nowy = point.y;
-        } catch (IOException e) {
-          System.out.println("[Server] Error in opening Socket");
-          System.exit(-1);
-        }
-      }
-      else {
-        try{
-          line = in.readLine(); //read input from client
-          if(line == null) {
-            System.out.println("[Server] Client disconnected.");
-            break;
-          }
-          //System.out.println("[Client] " + line); //print whatever we get from client
-
-          if(line.length() == 1) {
-            //TODO implement keyboard input
-          }
-
-          //input will come in x,y format if user moves mouse on mousepad
-          if(line.contains(",")) {
-            float movex = Float.parseFloat(line.split(",")[0]);//extract movement in x direction
-            float movey = Float.parseFloat(line.split(",")[1]);//extract movement in y direction
-            nowx += movex;
-            nowy += movey;
-
-            if(nowx < 0)
-              nowx = 0;
-            else if(nowx > width)
-              nowx = width;
-
-            if(nowy < 0)
-              nowy = 0;
-            else if(nowy > height)
-              nowy = height;
-
-            robot.mouseMove((int)(nowx),(int)(nowy));//Move mouse pointer to new location
-          }
-          //handle clicks
-          else if(line.contains("left_click_down")) {
-            System.out.println("[Server] Received left click down.");
-            robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
-          }
-          else if(line.contains("left_click_up")) {
-            System.out.println("[Server] Received left click up.");
-            robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
-          }
-          else if(line.contains("right_click_down")) {
-            System.out.println("[Server] Received right click down.");
-            robot.mousePress(InputEvent.BUTTON3_DOWN_MASK);
-          }
-          else if(line.contains("right_click_up")) {
-            System.out.println("[Server] Received right click up.");
-            robot.mouseRelease(InputEvent.BUTTON3_DOWN_MASK);
-          }
-          //handle disconnections
-          else if(line.equalsIgnoreCase("exit")) {
-            isConnected=false;
-            //Close client socket
-            client.close();
-          }
-          else if(line.equalsIgnoreCase("serverExit")) {
-            isConnected=false;
-            client.close();
-            System.out.println("[Server] Shutting down.");
-            running = false;
-          }
-        } catch (IOException e) {
-          System.out.println("[Server] Read failed");
-          System.exit(-1);
-        }
-      }
-    }
-  }
+	private int count;
+	
+	private long startTime;
+	private long endTime;
+	
+	private float nowx;
+	private float nowy;
+ 
+	public ComputerPort() {
+		selectPort();
+		init();
+	}
+	
+	private void selectPort() {
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Please select a socket (e.g. 9000): ");
+		serverPort = Integer.parseInt(scan.next());
+		scan.close();
+		System.out.println("[Server] Port: " + serverPort);
+	}
+	
+	private void init() {
+		try{
+			System.out.println("[Server] INIT: Socket: " + serverPort);
+			Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+			width = screenSize.width;
+			height = screenSize.height;
+	    	robot = new Robot(); //create robot for automating mouse movements/clicks
+			server = new ServerSocket(serverPort); //create a server socket on port
+			Point point = MouseInfo.getPointerInfo().getLocation(); //Get current mouse position
+			nowx = point.x;
+			nowy = point.y;
+			startTime = System.nanoTime();
+		}catch (IOException e) {
+			System.out.println("[Server] Error in opening Socket. Make sure no other program is using this socket!");
+			System.exit(-1);
+		}catch (AWTException e) {
+			System.out.println("[Server] Error in creating robot instance");
+			System.exit(-1);
+		}
+	}
+	
+	public void updateState() {
+		running = true;
+		//read input from client while it is connected
+	    while(running) {
+	    	if(!isConnected) {
+	    		try {
+	    			client = server.accept(); //listens for a connection to be made to this socket and accepts it
+	    			in = new BufferedReader(new InputStreamReader(client.getInputStream())); //the input stream where data will come from client
+	    		    isConnected = true;
+	    		    Point point = MouseInfo.getPointerInfo().getLocation(); //Get current mouse position
+	    			nowx = point.x;
+	    			nowy = point.y;
+	    			System.out.println("[Server] Client connected.");
+	    		} catch (IOException e) {
+	    			System.out.println("[Server] Error in opening Socket");
+	    			System.exit(-1);
+	    		}
+	    	}
+	    	else {
+		        try{
+					line = in.readLine(); //read input from client
+					if(line == null) {
+						System.out.println("[Server] Client disconnected.");
+						break;
+					}
+					count++;
+					endTime = System.nanoTime();
+					
+					if(endTime > startTime + 1000000000) {
+						System.out.println("[Server] MPS: " + (float) count / ((endTime - startTime) / 1000000000));
+						count = 0;
+						startTime = System.nanoTime();
+					}
+					//System.out.println("[Client] " + line); //print whatever we get from client
+					
+					//input will come in x,y format if user moves mouse on mousepad
+					if(line.contains(",")) {
+						float movex = Float.parseFloat(line.split(",")[0]);//extract movement in x direction
+						float movey = Float.parseFloat(line.split(",")[1]);//extract movement in y direction
+						nowx += movex;
+						nowy += movey;
+						
+						if(nowx < 0)
+							nowx = 0;
+						else if(nowx > width)
+							nowx = width;
+						
+						if(nowy < 0)
+							nowy = 0;
+						else if(nowy > height)
+							nowy = height;
+						
+						robot.mouseMove((int)(nowx),(int)(nowy));//Move mouse pointer to new location
+					}
+					//handle clicks
+					else if(line.contains("left_click_down")) {
+						System.out.println("[Server] Received left click down.");
+						robot.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+					}
+					else if(line.contains("left_click_up")) {
+						System.out.println("[Server] Received left click up.");
+						robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+					}
+					else if(line.contains("right_click_down")) {
+						System.out.println("[Server] Received right click down.");
+						robot.mousePress(InputEvent.BUTTON3_DOWN_MASK);
+					}
+					else if(line.contains("right_click_up")) {
+						System.out.println("[Server] Received right click up.");
+						robot.mouseRelease(InputEvent.BUTTON3_DOWN_MASK);
+					}
+					//handle disconnections
+					else if(line.equalsIgnoreCase("exit")) {
+						isConnected = false;
+						//Close client socket
+						client.close();
+		    			System.out.println("[Server] Client disconnected.");
+					}
+					else if(line.equalsIgnoreCase("serverExit")) {
+						isConnected=false;
+						client.close();
+						System.out.println("[Server] Shutting down.");
+						running = false;
+					}
+		        } catch (IOException e) {
+					System.out.println("[Server] Read failed");
+					System.exit(-1);
+		        }
+	      	}
+	    }
+	}
 }
